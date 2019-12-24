4482aeda4be8f3ba8ec9cb9c36aa67e9693a9cc2
aayush raj <42403809+araj1108@users.noreply.github.com>
2019-12-24 10:43:02 +0530

    Create Divisible Sum Pairs.cpp

---
+++ Competitive Coding Solutions/Hackerrank/Divisible Sum Pairs.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n,k,count=0;
+    cin>>n>>k;
+    int a[n];
+    for(int i=0;i<n;i++)
+    {
+        cin>>a[i];
+    }
+    for(int i=0;i<n;i++)
+    {
+        for(int j=i+1;j<n;j++)
+        {
+            if((a[i]+a[j])%k==0)
+            {
+                count++;
+            }
+        }
+    }
+    cout<<count;
+}
